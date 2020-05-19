a4de9e6018cd86808890896f0d53864bc3cbf7ae
alfmunny <alfmunny@gmail.com>
2020-05-19 22:34:43 +0200

    BST deletion added

--- algorithms-4th/algs4/searching/BST.java
+++ algorithms-4th/algs4/searching/BST.java
@@ -6,7 +6,7 @@ public class BST<Key extends Comparable<Key>, Value> {
         private Node left, right;
         private int N;
 
-        public Node(Key key, Value value, int N) {
+        public Node(Key key, Value val, int N) {
             this.key = key;
             this.val = val;
             this.N = N;
@@ -22,7 +22,7 @@ public class BST<Key extends Comparable<Key>, Value> {
         }
 
         public Value get(Key key) {
-          return get(root, key)
+          return get(root, key);
         }
 
         private Value get(Node x, Key key) {
@@ -45,6 +45,46 @@ public class BST<Key extends Comparable<Key>, Value> {
 
         public void put(Key key, Value val) {
             root = put(root, key, val);
+        }
+
+        public Node min(Node x) {
+            if (x.left == null) return x;
+            return min(x.left);
+        }
+
+        public void deleteMin() {
+            root = deleteMin(root);
+        }
+
+        private Node deleteMin(Node x) {
+            if (x.left == null) return x.right;
+             x.left = deleteMin(x.left);
+             x.N = size(x.right) + size(x.left) + 1;
+             return x;
+        }
+
+        public void delete(Key key) {
+            root = delete(root, key);
+        }
+
+        private Node delete(Node x, Key key) {
+            if (x == null) return null;
+            int cmp = key.compareTo(x.key);
+
+            if (cmp < 0) delete(x.left, key);
+            else if (cmp > 0) delete(x.right, key);
+            else {
+                if (x.left == null) return x.right;
+                else if (x.right == null) return x.left;
+                else {
+                    Node t = x;
+                    x = min(x.right);
+                    x.right = deleteMin(x.right);
+                    x.left = t.left;
+                }
+            }
+            x.N = size(x.left) + size(x.right) + 1;
+            return x;
         }
     }
 }
