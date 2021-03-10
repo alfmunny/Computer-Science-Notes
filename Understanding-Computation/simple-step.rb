d3590e19c818cf843731b3bcd902883c59dc2369
alfmunny <alfmunny@gmail.com>
2021-03-10 20:37:26 +0100

    Sequence added

--- Understanding-Computation/simple-step.rb
+++ Understanding-Computation/simple-step.rb
@@ -183,6 +183,30 @@ class If < Struct.new(:condition, :consequence, :alternative)
   end
 end
 
+class Sequence < Struct.new(:first, :second)
+  def to_s
+    "#{first}: #{second}"
+  end
+
+  def inspect
+    "«#{self}»" 
+  end
+
+  def reducible?
+    true
+  end
+
+  def reduce(environment)
+    case first
+    when DoNothing.new
+      [second, environment]
+    else
+      reduced_first, reduced_environment = first.reduce(environment)
+      [Sequence.new(reduced_first, second), reduced_environment]
+    end
+  end
+end
+
 class Machine < Struct.new(:statement, :environment)
   def step
     self.statement, self.environment = statement.reduce(environment)
@@ -198,10 +222,9 @@ class Machine < Struct.new(:statement, :environment)
 end
 
 Machine.new(
-  If.new(
-    Variable.new(:x),
-    Assign.new(:y, Number.new(1)),
-    Assign.new(:y, Number.new(2))
+  Sequence.new(
+    Assign.new(:x, Add.new(Number.new(1), Number.new(1))), 
+    Assign.new(:y, Add.new(Variable.new(:x), Number.new(3)))
   ),
-  { x: Boolean.new(true) }
+  { }
 ).run
